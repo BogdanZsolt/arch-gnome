e617256783065db789c67d56fdce14e211999404
BogdanZsolt <bogdan.zsolt.m@gmail.com>
2019-11-18 07:37:06 +0100

    New setup

--- 100-display-manager-and-desktop-v1.sh
+++ 100-display-manager-and-desktop-v1.sh
@@ -23,14 +23,15 @@ echo "#################################################"
 sudo pacman -Syyu --noconfirm
 
 #installing displaymanager or login manager
-sudo pacman -S --noconfirm --needed lightdm
-sudo pacman -S --noconfirm --needed lightdm-gtk-greeter lightdm-gtk-greeter-settings
+sudo pacman -S --noconfirm --needed gdm
+#sudo pacman -S --noconfirm --needed lightdm
+#sudo pacman -S --noconfirm --needed lightdm-gtk-greeter lightdm-gtk-greeter-settings
 
 #installing desktop environment
-sudo pacman -S gnome gnome-extra --noconfirm --needed
+sudo pacman -S gnome --noconfirm --needed
 
 #enabling displaymanager or login manager
-sudo systemctl enable lightdm.service -f
+sudo systemctl enable gdm.service -f
 sudo systemctl set-default graphical.target
 
 
