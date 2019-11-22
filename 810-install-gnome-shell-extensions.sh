0fcd8d873d8c2c13124ef2a7cd969dd793f0acd7
BogdanZsolt <bogdan.zsolt.m@gmail.com>
2019-11-22 15:51:27 +0100

    New config

--- 810-install-gnome-shell-extensions.sh
+++ 810-install-gnome-shell-extensions.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
 
-sudo pacman -Syyu --noconfirm
-sudo trizen -S --noconfirm --needed  
-sudo pacman -S --noconfirm --needed 
+sudo pacman -Syyu --noconfirm 
+sudo pacman -S --noconfirm --needed gnome-shell-extension-dash-to-dock
+sudo pacman -S --noconfirm --needed gnome-shell-extension-auto-move-windows
+sudo pacman -S --noconfirm --needed gnome-shell-extension-clipboard-indicator
+sudo pacman -S --noconfirm --needed gnome-shell-extension-freon
+sudo pacman -S --noconfirm --needed gnome-shell-extension-gsconnect
+sudo pacman -S --noconfirm --needed gnome-shell-extension-window-list
+
 
 ##########################################################################################
 #                                                                                        #
